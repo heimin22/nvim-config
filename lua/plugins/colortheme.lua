c36ef3303c64e7e9c3b6a79ed8247e416b2f457b
heimin22 <fyketonel@gmail.com>
2025-09-05 17:05:31 +0800

    colortheme changes creating transparency effect, some lualine changes and dashboard logo too.

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -3,7 +3,19 @@ return {
   {
     "projekt0n/github-nvim-theme",
     config = function()
-      require("github-theme").setup({})
+      require("github-theme").setup({
+        options = {
+          transparent = true,
+          terminal_colors = true,
+          dim_inactive = true,
+          module_default = true,
+          styles = {
+            commentrs = "italic",
+            keywords = "bold",
+            types = "italic,bold",
+          },
+        },
+      })
       vim.cmd("colorscheme github_dark_default")
     end,
   },
